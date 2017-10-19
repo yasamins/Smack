2877f649fdab14fcf3d37f053a4ed1536e0012a2
Yasamin Sa <yasamin@jongla.com>
2017-10-19 15:06:35 +0300

    avatarpicker

---
+++ Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,45 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by Yasamin Sa on 08/10/2017.
+//  Copyright © 2017 Yasamin Sa. All rights reserved.
+//
+
+import UIKit
+//in order to use collectionview we gonna need these protocols
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate,
+    UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    //Outlets
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //number of assets from 0 to 27
+        return 28
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,16 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Yasamin Sa on 18/10/2017.
+//  Copyright © 2017 Yasamin Sa. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+}
